ff1d347a60619fa0040e4d58f1c7a06610970a5e
Gustavo Amorim <gamorim@informa.com.br>
2021-08-20 17:14:53 -0300

    express setup

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,16 @@ require('dotenv-safe').config();
 import express from 'express';
 import { userRoutes } from './views/userRoutes';
 import { productRoutes } from './views/productRoutes';
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const port = process.env.PORT || 3000;
 const server = express();
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+server.use(cookieParser());
+server.use(session({ secret: process.env.SECRET }));
 server.use('/v1/user', userRoutes);
 server.use('/v1/product', productRoutes);
 
